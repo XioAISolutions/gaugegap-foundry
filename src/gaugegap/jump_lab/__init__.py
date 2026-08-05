fe17fa13ebae9caf2db01d7317b1782b4d62bb1f
PenguineWalkOS <slavazeph@gmail.com>
2026-08-05 13:10:33 -0400

    Expose Jump Lab v0.2 benchmark and report APIs

--- src/gaugegap/jump_lab/__init__.py
+++ src/gaugegap/jump_lab/__init__.py
@@ -1,19 +1,26 @@
 """GaugeGap Jump Lab: experience-to-axiom discovery experiments."""
 
 from .axiom import compile_local_equivalence_axiom, verify_local_equivalence
+from .benchmark import run_benchmark
 from .contracts import Intervention, Observation, Transition
 from .elevator import EinsteinElevatorWorld
-from .executive import AbductiveExecutive
+from .executive import AbductiveExecutive, ExecutiveConfig
+from .report import render_benchmark_html, render_discovery_html, write_html
 from .salience import SalienceCandidate, SalienceController
 
 __all__ = [
     "AbductiveExecutive",
     "EinsteinElevatorWorld",
+    "ExecutiveConfig",
     "Intervention",
     "Observation",
     "SalienceCandidate",
     "SalienceController",
     "Transition",
     "compile_local_equivalence_axiom",
+    "render_benchmark_html",
+    "render_discovery_html",
+    "run_benchmark",
     "verify_local_equivalence",
+    "write_html",
 ]
